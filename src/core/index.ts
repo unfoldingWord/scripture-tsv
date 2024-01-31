50c501422c44aee8e215650478a378ab8b5fcd05
kintsoogi <kintsoogii@proton.me>
2024-01-31 11:31:07 -0600

    Export the markdownToHTML function

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,7 @@
+import { markdownToHTML } from './markdownToHTML'
+
 export { default as useTsvData } from './hooks/useTsvData'
 export { default as flattenTsvObject } from './flattenTsvObject'
 export * as tsvDataActions from './tsvDataActions'
 export * as tsvRowUtils from './tsvRowUtils'
+export { markdownToHTML }
